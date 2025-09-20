e916a005fe8ecaa833d7df13388fe8c42244b75f
Ali Hasanov <nan.fgbs@gmail.com>
2025-09-20 15:15:00 +0400

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-import Login from './Login'
 
 function App() {
 
+  const [firstName, setFirstName] = useState('Ali');
+  const [lastName, setLastName] = useState('Alisoy');
+
+  const changeLastname = ()=>{
+    setLastName('Besirli')
+  }
+
+  const [name, setName] = useState(["name1", "name2", "name3", "name4", "name5"]);
+
+
   return (
-    <div className='flex flex-col gap-5'>
-      <Login/>
-      <hr />
-      <Login/>
+    <div className='flex flex-col text-2xl items-center'>
+      <div className='flex flex-col items-center mb-5'>
+        <p>{firstName}</p>      
+        <p>{lastName}</p> 
+      </div> 
+      <div className=''>
+        <button onClick={()=>{setFirstName("Mehemmed")}} className='rounded p-2 px-3 bg-slate-600 text-center text-white'>Change Name</button>
+        <button onClick={changeLastname} className='ml-3 rounded p-2 px-3 bg-slate-600 text-center text-white'>Change Lastname</button>
+      </div>   
+
+      <div className='flex flex-col items-center mt-5'>
+        {name.map((name, index) => (
+          <div key={index}>{name}</div>
+        ))}
+      </div>  
     </div>
   )
 }
